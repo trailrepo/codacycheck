b4345a9fbf9151a07c50c26c08564ecbe2fef54a
nareshbabur <nareshbabur@GCDTP004.GADAGET.COM>
2019-08-27 16:17:20 +0530

    TEST

---
+++ src/mypackage/test.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class test {
+public static void main(String[] args) {
+	int a=10;
+	boolean s=true;
+	while (s) {
+		
+	}
+	if(true)
+	{
+		System.out.println();
+	}
+}
+}
